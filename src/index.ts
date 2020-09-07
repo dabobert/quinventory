8f84fcc59091c6e9a91ea2a65081d54aede8bc75
Robert Jenkins <bobert@gmail.com>
2020-09-06 22:38:10 -0400

    server boots again

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-const express = require("express");
-const app     = express();
+import express from "express";
+const  app     = express();
 
-const port    = 8080; //default
+const port = 8080; // default
 app.get("/", (req, res) => {
   res.send("Welcome to QuInventory");
 });
 
 app.listen(port, () => {
+  // tslint:disable-next-line:no-console
   console.log(`server started on at http://localhost:${port}`);
 });
